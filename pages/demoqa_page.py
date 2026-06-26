37745ccd0fc116858918e9d57b99a3795a797627
kamiye <kamiye31@gmail.com>
2026-06-26 14:48:28 +0100

    refactor: update book row locator and improve visibility checks for book presence

--- pages/demoqa_page.py
+++ pages/demoqa_page.py
@@ -8,7 +8,7 @@ class DemoQAPage:
         self.login_submit = page.locator("button#login")
         self.logged_in_username = page.locator("#userName-value")
         self.search_input = page.locator("input#searchBox")
-        self.book_rows = page.locator(".rt-td a")
+        self.book_rows = page.get_by_role("link").filter(has_text="")
 
     def navigate_to_bookstore(self):
         self.page.goto("https://demoqa.com/books", wait_until="commit")
@@ -41,19 +41,18 @@ class DemoQAPage:
 
     def get_visible_book_count(self):
         try:
-            self.book_rows.first.wait_for(state="visible", timeout=10000)
+            self.page.get_by_role("link", name="Git Pocket Guide").wait_for(
+                state="visible", timeout=10000
+            )
         except:
             pass
-        return self.book_rows.count()
+        return self.page.locator(".rt-tr-group .rt-td a").count()
 
     def is_book_visible(self, title):
         try:
-            self.book_rows.first.wait_for(state="visible", timeout=10000)
+            self.page.get_by_role("link", name=title).wait_for(
+                state="visible", timeout=10000
+            )
+            return True
         except:
             return False
-        count = self.book_rows.count()
-        for i in range(count):
-            row_text = self.book_rows.nth(i).inner_text()
-            if title in row_text:
-                return True
-        return False
